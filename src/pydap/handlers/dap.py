7baddb5201185230125de3ce08faf8de6bc6b8b6
Roberto De Almeida <roberto@dealmeida.net>
2012-11-03 21:48:06 -0700

    Initial import from Bitbucket

---
+++ src/pydap/handlers/dap.py
@@ -0,0 +1,237 @@
+from urlparse import urlsplit, urlunsplit
+
+import numpy as np
+import requests
+
+from pydap.lib import encode, combine_slices, fix_slice, hyperslab, START_OF_SEQUENCE, END_OF_SEQUENCE
+from pydap.handlers.lib import ConstraintExpression, BaseHandler
+from pydap.responses.dods import typemap
+from pydap.parsers.dds import build_dataset
+from pydap.parsers.das import add_attributes
+
+
+class DAPHandler(BaseHandler):
+    def __init__(self, url):
+        scheme, netloc, path, query, fragment = urlsplit(url)
+        ddsurl = urlunsplit(
+                (scheme, netloc, path + '.dds', query, fragment))
+        dasurl = urlunsplit(
+                (scheme, netloc, path + '.das', query, fragment))
+
+        dds = requests.get(ddsurl).text
+        das = requests.get(dasurl).text
+
+        self.dataset = build_dataset(dds)
+        add_attributes(self.dataset, das)
+
+
+class BaseProxy(object):
+    def __init__(self, baseurl, id, dtype, shape, slice_=None):
+        self.baseurl = baseurl
+        self.id = id
+        self.dtype = np.dtype(dtype)
+        self.shape = shape
+        self.slice = slice_ or (slice(None),)
+
+    def __getitem__(self, index):
+        # build download url
+        index = combine_slices(self.slice, fix_slice(index, self.shape))
+        scheme, netloc, path, query, fragment = urlsplit(self.baseurl)
+        url = urlunsplit((
+                scheme, netloc, path + '.dods',
+                self.id + hyperslab(index) + '&' + query,
+                fragment)).rstrip('&')
+
+        # download and unpack data
+        r = requests.get(url)
+        dds, data = r.content.split('\nData:\n', 1)
+        
+        if self.shape:
+            # skip size packing
+            if self.dtype.char == 'S':
+                data = data[4:]
+            else:
+                data = data[8:]
+
+        # calculate array size
+        shape = tuple((s.stop-s.start)/s.step for s in index)
+        size = np.prod(shape)
+
+        if self.dtype == np.byte:
+            return np.fromstring(data[:size], 'B')
+        elif self.dtype.char == 'S':
+            out = []
+            for word in range(size):
+                n = np.fromstring(data[:4], '>I')  # read length
+                data = data[4:]
+                out.append(data[:n])
+                data = data[n + (-n % 4):]
+            return np.array(out, 'S')
+        else:
+            return np.fromstring(data, self.dtype).reshape(shape)
+
+    def __len__(self):
+        return self.shape[0]
+
+    def __iter__(self):
+        return iter(self[:])
+
+    # Comparisons return a boolean array
+    def __eq__(self, other): return self[:] == other
+    def __ne__(self, other): return self[:] != other
+    def __ge__(self, other): return self[:] >= other
+    def __le__(self, other): return self[:] <= other
+    def __gt__(self, other): return self[:] > other
+    def __lt__(self, other): return self[:] < other
+
+
+class SequenceProxy(object):
+
+    shape = ()
+
+    def __init__(self, baseurl, id, descr, selection=None, slice_=None):
+        self.baseurl = baseurl
+        self.id = id
+        self.descr = descr
+        self.selection = selection or []
+        self.slice = slice_ or (slice(None),)
+
+    def __iter__(self):
+        scheme, netloc, path, query, fragment = urlsplit(self.baseurl)
+        url = urlunsplit((
+                scheme, netloc, path + '.dods',
+                self.id + hyperslab(self.slice) + '&' + '&'.join(self.selection),
+                fragment)).rstrip('&')
+
+        # download and unpack data
+        r = requests.get(url, prefetch=False)
+
+        # strip dds response
+        marker = '\nData:\n'
+        buf = []
+        while ''.join(buf) != marker:
+            chunk = r.raw.read(1)
+            if not chunk:
+                break
+            buf.append(chunk)
+            buf = buf[-len(marker):]
+
+        return unpack_sequence(r, self.descr)
+
+    def __getitem__(self, key):
+        out = self.clone()
+
+        # return the data for a children
+        if isinstance(key, basestring):
+            out.id = '{id}.{child}'.format(id=self.id, child=key)
+            out.descr = apply_to_list(
+                    lambda l, key=key: [(k, v) for k, v in l if k==key][0], 
+                    out.descr)
+
+        # return a new object with requested columns
+        elif isinstance(key, list):
+            out.descr = apply_to_list(
+                    lambda l, key=key: [(k, v) for k, v in l if k in key],
+                    out.descr)
+
+        # return a copy with the added constraints
+        elif isinstance(key, ConstraintExpression):
+            out.selection.extend( str(key).split('&') )
+
+        # slice data
+        else:
+            if isinstance(key, int):
+                key = slice(key, key+1)
+            out.slice = combine_slices(self.slice, (key,))
+
+        return out
+
+    def clone(self):
+        return self.__class__(self.baseurl, self.id, self.descr,
+                self.selection[:], self.slice[:])
+
+    def __eq__(self, other): return ConstraintExpression('%s=%s' % (self.id, encode(other)))
+    def __ne__(self, other): return ConstraintExpression('%s!=%s' % (self.id, encode(other)))
+    def __ge__(self, other): return ConstraintExpression('%s>=%s' % (self.id, encode(other)))
+    def __le__(self, other): return ConstraintExpression('%s<=%s' % (self.id, encode(other)))
+    def __gt__(self, other): return ConstraintExpression('%s>%s' % (self.id, encode(other)))
+    def __lt__(self, other): return ConstraintExpression('%s<%s' % (self.id, encode(other)))
+
+
+def apply_to_list(func, descr):
+    """
+    Apply a function to a list inside a dtype descriptor.
+
+    """
+    if not isinstance(descr, tuple):
+        descr = ('', descr)
+        wrap = True
+    else:
+        wrap = False
+
+    name, dtype = descr
+    if isinstance(dtype, list):
+        dtype = func(dtype)
+    else:
+        dtype = apply_to_list(func, dtype)
+
+    if wrap:
+        return dtype
+    else:
+        return name, dtype
+
+
+def unpack_sequence(r, descr):
+    """
+    Unpack data from a sequence.
+
+    """
+    # numpy dtypes must be list of tuples, but we use single tuples to 
+    # differentiate children from sequences with only one child, ie,
+    # sequence['foo'] from sequence[['foo']]; here we convert descr to
+    # a proper dtype
+    sequence = isinstance(descr, list)
+    def fix(descr):
+        if isinstance(descr, tuple):
+            return [ (descr[0], fix(descr[1])) ]
+        else:
+            return descr
+    dtype = fix(descr)
+
+    # if there are no strings and no nested sequences we can
+    # unpack record by record easily
+    simple = all(not isinstance(v, list) and 'S' not in v
+            for k, v in dtype)
+    if simple:
+        dtype = np.dtype(dtype)
+        marker = r.raw.read(4)
+        while marker == START_OF_SEQUENCE:
+            rec = np.fromstring(r.raw.read(dtype.itemsize), dtype=dtype)[0]
+            if not sequence:
+                rec = rec[0]
+            yield rec
+            marker = r.raw.read(4)
+    else:
+        marker = r.raw.read(4)
+        while marker == START_OF_SEQUENCE:
+            rec = []
+            for name, d in dtype:
+                d = np.dtype(d)
+                if d.char == 'S':
+                    n = np.fromstring(r.raw.read(4), '>I')[0]
+                    rec.append(r.raw.read(n))
+                    r.raw.read(-n % 4)
+                elif d.char == 'V':
+                    if isinstance(descr, list):
+                        sub = [v for k, v in descr if k == name][0]
+                    else:
+                        sub = descr[1]
+                    rec.append(tuple(unpack_sequence(r, sub)))
+                else:
+                    data = np.fromstring(r.raw.read(d.itemsize), d)[0]
+                    rec.append(data)
+            if not sequence:
+                rec = rec[0]
+            yield tuple(rec)
+            marker = r.raw.read(4)
+
---
+++ src/pydap/responses/dds.py
@@ -0,0 +1,90 @@
+from pydap.model import *
+from pydap.responses.lib import BaseResponse
+
+
+INDENT = ' ' * 4
+
+typemap = {
+        'd': 'Float64',
+        'f': 'Float32',
+        'h': 'Int16',
+        'i': 'Int32', 'l': 'Int32', 'q': 'Int32',
+        'b': 'Byte',
+        'H': 'UInt16',
+        'I': 'UInt32', 'L': 'UInt32', 'Q': 'UInt32',
+        'B': 'Byte',
+        'S': 'String',
+        'U': 'String',
+}
+
+
+class DDSResponse(BaseResponse):
+    def __init__(self, dataset):
+        BaseResponse.__init__(self, dataset)
+        self.headers.extend([
+            ('Content-description', 'dods_dds'),
+            ('Content-type', 'text/plain; charset=utf-8'),
+        ])
+
+    def __iter__(self):
+        for line in dispatch(self.dataset):
+            yield line
+
+        if hasattr(self.dataset, 'close'):
+            self.dataset.close()
+
+
+def dispatch(var, level=0):
+    types = [
+            (DatasetType, structure('Dataset')),
+            (SequenceType, structure('Sequence')),
+            (GridType, grid),
+            (StructureType, structure('Structure')),
+            (BaseType, base),
+    ]
+
+    for class_, func in types:
+        if isinstance(var, class_):
+            return func(var, level)
+
+
+def structure(type):
+    def func(var, level=0):
+        yield '{indent}{type} {{\n'.format(indent=level*INDENT, type=type)
+
+        # Get the DDS from stored variables.
+        for child in var.children():
+            for line in dispatch(child, level+1):
+                yield line
+        yield '{indent}}} {name};\n'.format(indent=level*INDENT, name=var.name)
+    return func
+
+
+def grid(var, level=0):
+    yield '{indent}Grid {{\n'.format(indent=level*INDENT)
+
+    yield '{indent}Array:\n'.format(indent=(level+1)*INDENT)
+    for line in base(var.array, level+2):
+        yield line
+
+    yield '{indent}Maps:\n'.format(indent=(level+1)*INDENT)
+    for map_ in var.maps.values():
+        for line in base(map_, level+2):
+            yield line
+
+    yield '{indent}}} {name};\n'.format(indent=level*INDENT, name=var.name)
+
+
+def base(var, level=0):
+    if var.dimensions:
+        shape = ''.join(map('[{0[0]} = {0[1]}]'.format, zip(var.dimensions, var.shape)))
+    elif len(var.shape) == 1:
+        shape = '[{} = {}]'.format(var.name, var.shape[0])
+    else:
+        shape = ''.join(map(str, var.shape))
+
+    yield '{indent}{type} {name}{shape};\n'.format(
+            indent=level*INDENT,
+            type=typemap[var.dtype.char], 
+            name=var.name,
+            shape=shape)
